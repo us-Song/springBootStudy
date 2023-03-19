798a575f1a0a7033496b49e978cd21ca2771a9be
us-Song <dmltjr05@naver.com>
2023-03-19 22:40:25 +0900

    1

--- Java-Spring-study/spring핵심원리/core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ Java-Spring-study/spring핵심원리/core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -20,13 +20,13 @@ public class ApplicationContextExtendsFindTest
     AnnotationConfigApplicationContext ac = new
             AnnotationConfigApplicationContext(TestConfig.class);
 
-    @Test
-    @DisplayName("부모타입으로 조회시, 자식이 둘 이상 있으면, 중복오류가 발생한다")
-    void findBeanByParentTypeDuplicate(){
-        DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
-        assertThrows(NoUniqueBeanDefinitionException.class,
-                ()->ac.getBean(DiscountPolicy.class));
-    }
+//    @Test
+//    @DisplayName("부모타입으로 조회시, 자식이 둘 이상 있으면, 중복오류가 발생한다")
+//    void findBeanByParentTypeDuplicate(){
+//        DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
+////        assertThrows(NoUniqueBeanDefinitionException.class,
+////                ()->ac.getBean(DiscountPolicy.class));
+//    }
 
     @Test
     @DisplayName("부모타입으로 조회시, 자식이 둘 이상 있으면, 빈 이름을 지정하면 된다")
--- Java-Spring-study/spring핵심원리/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ Java-Spring-study/spring핵심원리/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -33,4 +33,12 @@ public class ConfigurationSingletonTest {
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
 
     }
+    
+    @Test
+    void configureationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean.getClass() = " + bean.getClass());
+    }
 }
