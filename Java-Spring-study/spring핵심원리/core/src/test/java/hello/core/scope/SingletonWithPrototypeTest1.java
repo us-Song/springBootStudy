1b3f5961ed1d3c3aff6bee1c6060fe5375b33568
us-Song <dmltjr05@naver.com>
2023-04-25 23:56:07 +0900

    20230425

--- Java-Spring-study/spring핵심원리/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ Java-Spring-study/spring핵심원리/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,12 +1,14 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -33,20 +35,16 @@ public class SingletonWithPrototypeTest1 {
         assertThat(count1).isEqualTo(1);
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-
-        private final PrototypeBean prototypeBean; //생성 시점에 주입
-
         @Autowired
-        private ClientBean(PrototypeBean prototypeBean, PrototypeBean prototypeBean1) {
-            this.prototypeBean = prototypeBean1;
-        }
+        private Provider<PrototypeBean> prototypeBeanObjectProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.get(); //컨테이너에서 찾아서 반환
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
