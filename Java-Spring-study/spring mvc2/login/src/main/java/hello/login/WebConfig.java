6b28a60400baa842b2731e545ebd9b3797e05480
us-Song <dmltjr05@naver.com>
2024-08-23 22:18:16 +0900

    new

---
+++ Java-Spring-study/spring mvc2/login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,62 @@
+package hello.login;
+
+import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
+import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**") //체크할 url
+                .excludePathPatterns("/", "/member/add", "/login", "logout", "/css/**",
+                        "/*.ico", "/error"); //제외할 url
+    }
+
+    //필터 설정
+//    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<Filter>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+
+        return filterFilterRegistrationBean;
+    }
+
+//    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<Filter>();
+        filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterFilterRegistrationBean.setOrder(2);
+        filterFilterRegistrationBean.addUrlPatterns("/*"); // 신규 기능 추가될 때마다  url 체크 할 수는 없으므로 필터에는 * 해놓고 필터 로직에 화이트 리스트로 관리하기
+
+        return filterFilterRegistrationBean;
+    }
+
+}
--- Java-Spring-study/spring mvc2/login/src/main/java/hello/login/web/HomeController.java
+++ Java-Spring-study/spring mvc2/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -83,9 +84,23 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV3Spring(
             @SessionAttribute(name = LOGIN_MEMBER, required = false) Member loginMember ,Model model) {
+        //이미 로그인 된 사용자를 찾을 때 사용 -> name으로 세션을 꺼내서 컨트롤러로 넘겨줌
+
+        //세션에 회원 데이터 없으면 홈화면
+        if(loginMember == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인 홈으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember , Model model) {
         //이미 로그인 된 사용자를 찾을 때 사용 -> name으로 세션을 꺼내서 컨트롤러로 넘겨줌
 
         //세션에 회원 데이터 없으면 홈화면
