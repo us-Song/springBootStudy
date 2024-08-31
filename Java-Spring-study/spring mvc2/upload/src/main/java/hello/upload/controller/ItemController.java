ab57688f859b045a511650c422911d6888fb4234
us-Song <dmltjr05@naver.com>
2024-09-01 01:04:13 +0900

    new

---
+++ Java-Spring-study/spring mvc2/upload/src/main/java/hello/upload/controller/ItemController.java
@@ -0,0 +1,91 @@
+package hello.upload.controller;
+
+import hello.upload.domain.Item;
+import hello.upload.domain.ItemRepository;
+import hello.upload.domain.UploadFile;
+import hello.upload.file.FileStore;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemRepository itemRepository;
+    private final FileStore fileStore;
+
+    @GetMapping("/items/new")
+    public String newItem(@ModelAttribute ItemForm form) {
+        return "item-form";
+    }
+
+    @PostMapping("/items/new")
+    public String saveItem(@ModelAttribute ItemForm form, RedirectAttributes redirectAttributes) throws IOException {
+        UploadFile attachFile = fileStore.storeFile(form.getAttachFile());
+        List<UploadFile> storeImageFiles = fileStore.storeFiles(form.getImageFiles());
+
+        //데이터베이스에 저장 (보통 파일 자체는 aws 같은 곳에 저장하고 , 디비에는 파일 이름, 경로 정도나 저장함
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setAttachFile(attachFile);
+        item.setImageFiles(storeImageFiles);
+        itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", item.getId());
+
+        return "redirect:/items/{itemId}";
+    }
+
+    @GetMapping("/items/{id}")
+    public String items(@PathVariable Long id, Model model) {
+        Item item = itemRepository.findById(id);
+        model.addAttribute("item", item);
+        return "item-view";
+    }
+
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+        //item-view.html에서 호출이된다.
+        log.info("downloadImage start");
+        // "file:" = 내부 파일에 접근 - > UrlResource 에서 파일에 직접 접근해서 리소스를 가져와서 반 -> 파일 리소스를 반환해서 html에서 이미지 태그를 사용해서 화면에서 보이는 것
+        return new UrlResource("file:" + fileStore.getFullPath(filename));
+    }
+
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        //item-view.html 에서 파일 링크 클릭시 호출된다
+        log.info("downloadAttach start");
+        Item item = itemRepository.findById(itemId);
+        String storeFileName = item.getAttachFile().getStoreFileName();//실제 저장 이름
+        String uploadFileName = item.getAttachFile().getUploadFileName();//사용자 저장 이름
+
+        UrlResource resource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
+
+        log.info("uploadFileName={}", uploadFileName);
+
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8); //한글 안깨지게 인코딩
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+        // contentDisposition 은 규약임, 이게 사용자 실제 파일 저장이름이니까 사용자 업로드시 이름으로 해주는게 좋음 - 브라우저가 이 헤다를 보고 다운로드 해야된다고 인식
+        //attachment : 다운로드 해야되는 거라고 명시
+        //attchment; filename - '다른 이름으로 저장'을 하려면 filename 같이 넣어준다
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(resource);
+    }
+}
